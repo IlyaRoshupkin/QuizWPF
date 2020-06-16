b3e2a174eb93c1e5c071f1fa899c696944702573
Илья Рощупкин <roshupkin.ilya2012@yandex.ru>
2020-06-16 21:08:59 +0300

    added Question and QuestionDb classes

---
+++ Quiz/Question.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Quiz
+{
+    class Question
+    {
+        /// <summary>
+        /// Picture for "View"
+        /// </summary>
+        public BitmapImage Picture { get; set; }
+
+        /// <summary>
+        /// Answer for Picture
+        /// </summary>
+        public string Answer { get; set; }
+
+        /// <summary>
+        /// Question Creation
+        /// </summary>
+        /// <param name="PathBitmapImageSource">A path to a picture</param>
+        /// <param name="AnswerSource">An answer</param>
+        
+        public Question(string PathBimapImageSource, string AnswerSource)
+        {
+            this.Picture = new BitmapImage();
+            this.Picture.BeginInit();
+            this.Picture.UriSource = new Uri(PathBimapImageSource, UriKind.Relative);
+            this.Picture.EndInit();
+            this.Answer = AnswerSource;
+        }
+    }
+}
---
+++ Quiz/QuestionDataBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class QuestionDataBase
+    {
+        /// <summary>
+        /// The question collection
+        /// </summary>
+        private List<Question> dB;
+
+        /// <summary>
+        /// Current question index
+        /// </summary>
+        private int index;
+
+        /// <summary>
+        /// The current question
+        /// </summary>
+        public Question CurrentQuestion
+        {
+            get
+            {
+                index++;
+                return dB[index % dB.Count];
+            }
+        }
+
+        /// <summary>
+        /// "QuestionDb" creation
+        /// </summary>
+        public QuestionDataBase()
+        {
+            this.dB = new List<Question>();
+            this.index = 0;
+            //Debug.WriteLine(@"..\..\Images\answers.txt");
+
+            var dateFile = File.ReadAllLines(@"..\..\Images\answers.txt");
+
+            foreach (var e in dateFile)
+            {
+                var args = e.Split('|');
+                dB.Add(new Question(args[0], args[1]));
+            }
+        }
+    }
+}
